15b313baa574117875883c2da782b86d7b0d80ce
dinaolmelak <dinaolmelak@gmail.com>
2022-02-15 16:28:56 -0600

    Setting up CollectionView and Cells

---
+++ Scholars Academy/SheetCell.swift
@@ -0,0 +1,17 @@
+//
+//  SheetCell.swift
+//  Scholars Academy
+//
+//  Created by Dinaol Tadesse on 15/02/2022.
+//
+
+import UIKit
+
+class SheetCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var view: UIView!
+    @IBOutlet weak var cellTitle: UILabel!
+    @IBOutlet weak var cellDescription: UILabel!
+    
+}
--- Scholars Academy/SignInSheetsVC.swift
+++ Scholars Academy/SignInSheetsVC.swift
@@ -7,15 +7,32 @@
 
 import UIKit
 
-class SignInSheetsVC: UIViewController {
+class SignInSheetsVC: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
 
+    var test = ["Tutor", "Mentor","LSAMP"]
+    @IBOutlet weak var SignInSheetCollection: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        SignInSheetCollection.delegate = self
+        SignInSheetCollection.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
+    override func viewDidAppear(_ animated: Bool) {
+        SignInSheetCollection.reloadData()
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return test.count
+    }
     
-
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let collectionCell = SignInSheetCollection.dequeueReusableCell(withReuseIdentifier: "SheetCell", for: indexPath) as! SheetCell
+        collectionCell.cellTitle.text = test[indexPath.row]
+        collectionCell.view.layer.cornerRadius = 20
+        return collectionCell
+    }
+    
+    
     /*
     // MARK: - Navigation
 
