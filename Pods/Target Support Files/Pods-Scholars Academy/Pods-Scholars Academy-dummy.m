2ebe3664d477017c69c97e17c9e470dbb656573c
Getnet Mekuriyaw <melkamugetnet1@gmail.com>
2022-02-15 20:29:14 -0600

    Integrated firebase with the app

---
+++ Pods/Target Support Files/Pods-Scholars Academy/Pods-Scholars Academy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Scholars_Academy : NSObject
+@end
+@implementation PodsDummy_Pods_Scholars_Academy
+@end
---
+++ Pods/Target Support Files/Pods-Scholars Academy/Pods-Scholars Academy-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Scholars_AcademyVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Scholars_AcademyVersionString[];
+
